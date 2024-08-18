d53142a2cdae808d2a222fe1b1853ad8275c9a81
kiran.gondi <kiran.gondi@planview.com>
2024-08-18 15:42:42 +0530

    Monostate in Singleton

---
+++ Creational/Singleton/SingletonImpFramework/Monostate/Monostate/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace DesignPatterns
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get=> age; set => age = value; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}:{Age}";
+        }
+    }
+
+    public class Program { 
+        public static void Main(string[] args)
+        {
+            var ceo1 = new CEO();
+            ceo1.Name = "Inmy";
+            ceo1.Age = 54;
+
+            var ceo2 = new CEO();
+            //Console.WriteLine(ceo1.ToString());
+            Console.WriteLine(ceo2.ToString());
+
+            Console.ReadLine();
+        }
+    }
+
+}
